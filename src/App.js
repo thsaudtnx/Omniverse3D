8aabd766e5c298c1f7240e64acf79bf905cf5912
Myungsoo Son <thsaudtnx@gmail.com>
2023-11-14 13:39:34 +0800

    Divide the Login and Game page

--- src/App.js
+++ src/App.js
@@ -5,19 +5,12 @@ import GamePage from "./pages/GamePage";
 function App() {
   return (
     <>
-      <Canvas>
-        <Sky sunPosition={[100, 100, 20]} />
-        <ambientLight intensity={0.5} />
-        <FPV />
-        <Physics>
-          <Player />
-          <Cubes />
-          <Ground />
-        </Physics>
-      </Canvas>
-      <div className='absolute centered cursor'>+</div>
-      <TextureSelector />
-      <Menu />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}/>
+          <Route path="/game" element={<GamePage />}/>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
