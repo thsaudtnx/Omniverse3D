6e6f5ce5ab3bf60327107f72a771349b741cc50e
Myungsoo Son <thsaudtnx@gmail.com>
2023-11-14 12:14:02 +0800

    Divide the Login and Game page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-import { Physics } from '@react-three/cannon';
-import { Sky } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
-import { Ground } from './components/Ground'
-import { Player } from './components/Player'
-import { FPV } from './components/FPV'
-import { Cubes } from './components/Cubes'
-import { TextureSelector } from './components/TextureSelector';
-import { Menu } from './components/Menu';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import GamePage from "./pages/GamePage";
 
 function App() {
   return (
-    <>
-      <Canvas>
-        <Sky sunPosition={[100, 100, 20]} />
-        <ambientLight intensity={0.5} />
-        <FPV />
-        <Physics>
-          <Player />
-          <Cubes />
-          <Ground />
-        </Physics>
-      </Canvas>
-      <div className='absolute centered cursor'>+</div>
-      <TextureSelector />
-      <Menu />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/game" element={<GamePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+//Enxuan you can make the login page here!
+
+const LoginPage = () => {
+  return (
+    <>
+      LoginPage
+    </>
+  );
+}
+
+export default LoginPage;
