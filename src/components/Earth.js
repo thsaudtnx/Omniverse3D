75a02626f32db1069b1ccda344ead5daacd05305
Ladeece1 <enxuanl@gmail.com>
2023-11-17 17:42:29 +0800

    solar System

--- src/components/Earth.js
+++ src/components/Earth.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef, useState } from 'react';
-
 import EarthDayMap from '../assets/8k_earth_daymap.jpg';
 import EarthNormalMap from '../assets/8k_earth_normal_map.jpg';
 import EarthCloudsMap from '../assets/8k_earth_clouds.jpg';
