cfe88a0304f6b685b6bc70d1608b53ca14060c76
Myungsoo Son <thsaudtnx@gmail.com>
2024-02-19 00:24:52 +0800

    Add new columns to the continents database

--- src/components/earth/Map.js
+++ src/components/earth/Map.js
@@ -2,9 +2,12 @@ import React from 'react'
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 const Map = ({current}) => {
+
+  const googleMapApiKey = process.env.REACT_APP_GOOGLE_MAP_API_KEY
+
   return (
     <LoadScript
-      googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY}
+      googleMapsApiKey={googleMapApiKey}
     >
       <GoogleMap
         mapContainerStyle={{
@@ -12,19 +15,19 @@ const Map = ({current}) => {
           height: '350px'
         }}
         center={{
-          lat: current ? current.latitude : 0, 
-          lng: current ? current.longitude : 0
+          lat: current.latMap, 
+          lng: current.lngMap,
         }}
         zoom={1.5}
       >
         <Marker 
           position={{ 
-            lat: current ? current.latitude : 0, 
-            lng: current ? current.longitude : 0, 
+            lat: current.latMap, 
+            lng: current.lngMap, 
           }} 
           label={{
             color : 'white',
-            text : current ? current.name : 'World',
+            text : current.name,
             fontWeight: 'bold', 
             fontSize: '14px',
           }}
